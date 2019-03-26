6f4448c7176b7f5946dd5807a15328d3e1af71c9
Oscar Garcia <oscar.github@linaresdigital.com>
2019-03-26 15:32:20 +0100

    Traduciendo textos al castellano

--- public/index.php
+++ public/index.php
@@ -1,14 +1,12 @@
 <?php
 
-use \Psr\Http\Message\ResponseInterface as Response;
-use \Psr\Http\Message\ServerRequestInterface as Request;
-
 require '../vendor/autoload.php';
 
 $app = new \Slim\App();
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+$app->get('/{nombre}', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response) {
+    $response->getBody()->write(
+        "Hola, ". htmlspecialchars($request->getAttribute('nombre'))
+    );
 
     return $response;
 });
